dc64d8f2bcb8519c3d34191d273db0358ddac406
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-02 22:16:16 +0530

    commit to master

---
+++ assignment16.c
@@ -0,0 +1,29 @@
+//THIS IS PROGRAM TO READ THREE NOS. AND PRINT MAX
+#include<stdio.h>
+int main(){
+
+    int a, b , c;
+
+    printf("enter the value of a,b,c respectively");
+
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a>b){
+
+        if(a>c){
+            printf("a");
+        }
+        else{
+            printf("c");
+        }
+    }
+    else if(b>c){
+        printf("b");
+    }
+    else{
+        printf("c");
+    }
+
+    return 0;
+
+}
