c9c9e55f294548c3e8f6b99875b5f5169f82afbf
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-04 21:13:56 +0530

    commit

--- passignment6.c
+++ passignment6.c
@@ -1,1 +1,23 @@
-// this is program to swap two variables without using third variable
+// this is 
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input values from the user
+    printf("Enter two numbers a and b \n");
+    scanf("%d %d", &a, &b);
+    
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    
+    
+    a = a + b; 
+    b = a - b; 
+    a = a - b; 
+    
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
+
