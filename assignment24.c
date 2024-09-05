f27a8ca66391425bc53ed64e52b58d154486d0ec
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-05 16:45:56 +0530

    commit

---
+++ assignment24.c
@@ -0,0 +1,60 @@
+//    PROGRAM TO FIND THE VALUE OF Y USING 
+//    
+//    1+x where n=1
+//   1+x/n where n=2
+//   Y(x,n) = 1 +xn 
+//    when n=3
+//   1 + nx when n>3 or n<1
+
+
+#include <stdio.h>
+int main() {
+
+    int x,y,n;
+
+    printf("enter the value of n\n");
+
+    scanf("%d", &n);
+
+    switch (n)
+    {
+    case 1: printf("ENTER THE VALUE OF X AND Y");
+             
+             scanf("%d%d", &x,&y);
+
+             y= 1+x ;
+
+             printf("value of y is %d" ,y);
+        break;
+
+    
+    case 2:  printf("ENTER THE VALUE OF X AND Y");
+             
+             scanf("%d%d", &x,&y);
+
+             y= 1+(x/n) ;
+
+             printf("value of y is %d" ,y);
+        break;
+
+    case 3:  printf("ENTER THE VALUE OF X AND Y");
+             
+             scanf("%d%d", &x,&y);
+
+             y= 1+(n*x) ;
+
+             printf("value of y is %d" ,y);
+        break;    
+
+    }
+
+    if ((n>3)!!(n<1))
+    {
+        printf("ENTER THE VALUE OF X AND Y\n");
+
+        scanf("")
+    }
+    
+    
+    return 0;
+}
