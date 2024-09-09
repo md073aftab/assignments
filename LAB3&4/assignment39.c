caf99928d13eb6ebc8584db492316de27202f977
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-09 13:54:45 +0530

    zx z

--- LAB3&4/assignment39.c
+++ LAB3&4/assignment39.c
@@ -1,1 +1,29 @@
-// THIS IS 
+// THIS IS PROGRAM TO PRINT 1/1!+2/2!+3/3!+4/4!,………N
+
+#include <stdio.h>
+int main() {
+    
+    int a=1,b=1,c=1,n,num=0;
+
+    printf("enter n\n");
+    scanf("%d",&n);
+    
+    while (a<=n)
+    {
+        while (c<=a)
+        {
+            b=b*c;
+            c++ ;
+        }
+
+        num=num+a/b ;
+        a++ ;
+
+        
+        
+    }
+
+    printf("%d",num);
+    
+    return 0;
+}
