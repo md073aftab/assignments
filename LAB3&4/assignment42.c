3a8c976731ae09efbdffcc6299d9a5c670c11fd1
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-09 22:07:25 +0530

    commit

---
+++ LAB3&4/assignment42.c
@@ -0,0 +1,36 @@
+// THIS IS READ A NUMBER N AND PRINT IF N IS PRIME OR COMPOSITE
+
+#include <stdio.h>
+int main() {
+    int num,b;
+    int flag=1;
+
+    printf("enter num\n");
+    scanf("%d",&num);
+
+    if (num==2)
+    {
+        printf("2 is neither prime nor composite\n")
+    }
+    
+
+    for (b=2;b<num/2 && flag !=0 ;b++)
+    {
+        if (num%b==0)
+        {
+            flag=0 ;
+        }
+        
+    }
+    if (flag==1)
+    {
+        printf("%d is prime no",num);
+    }
+    else
+    {
+        printf("%d is composite");
+    }
+    
+    
+    return 0;
+}
---
+++ LAB3&4/assignment43.c
@@ -0,0 +1,12 @@
+// THIS IS WRITE A C PROGRAM TO CALCULATE THE COMMISSION FOR A SALES 
+// REPRESENTATIVE AS PER THE SALES AMOUNT GIVEN BELOW.
+// If sales<=Rs. 500, commission is 5%
+// If sales> but <=2000, commission is Rs. 35 plus 10% above Rs. 500
+// If sales>2000 but <=5000, commission is Rs. 185 plus 12% above Rs. 2000
+// If sales>5000, commission is 12.5%
+
+#include <stdio.h>
+int main() {
+    
+    return 0;
+}
