83535bc688ba7c68d79ca2a1b1017e9554ab80a6
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-12 14:20:25 +0530

    cammit changes

--- LAB3&4/assignment35.c
+++ LAB3&4/assignment35.c
@@ -1,22 +1,29 @@
-// THIS IS  program to READ A NUMBER N AND PRINT A SINGLE DIGIT ANSWER SHOWING SUM OF DIGIT OF 
-// 
+// READ A NUMBER N AND PRINT A SINGLE DIGIT ANSWER SHOWING SUM OF DIGIT OF 
+// N
+
 
 #include <stdio.h>
+
 int main() {
+    int n, num = 0, a;
+
     
-    int n,sum=0,a;
+    printf("Enter the value of n\n");
+    scanf("%d", &n);
 
-    printf ("enter the value of n\n");
-    scanf("%d",&n);
-
-    while (n>0)
-    {
-        a=n%10 ;
-        n=n/10 ;
-        sum=sum+a ;
+    
+    while (n > 0 || num >= 10) {
+        if (n == 0) {
+            n = num;
+            num = 0;
+        }
+        a = n % 10;
+        n = n / 10;
+        num = num + a;
     }
 
-    printf("%d",sum);
     
+    printf("The single-digit sum is: %d\n", num);
+
     return 0;
 }
