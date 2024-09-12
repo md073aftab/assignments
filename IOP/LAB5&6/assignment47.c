60c7979ae8ef4a0ce6a2ad9f2d02949bb5c43348
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-12 17:10:36 +0530

    commit

--- IOP/LAB5&6/assignment47.c
+++ IOP/LAB5&6/assignment47.c
@@ -7,6 +7,20 @@
 
 #include <stdio.h>
 int main() {
+    int i,j ;
+    char A,B,C,D,E;
+
+    
+    
+    for (i=1; i <=5; i++)
+    {
+        for (j=1; j<=i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+        
+    }
     
     return 0;
 }
---
+++ IOP/LAB5&6/assignment48.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int i,j;
+
+    for ( i = 1; i < 6; i++)
+    {
+        for ( j = 1; j <6; j++)
+        {
+            if (j<(6-i))
+            {
+                printf(" ");
+            }
+            else{
+                printf("%d",i+j-5);
+            }
+            
+        }
+        printf("\n");
+        
+    }
+    
+
+    return 0;
+}
