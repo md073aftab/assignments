ff2c7de56bb0a4b63ef7807339079bc489c836d2
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-19 16:18:26 +0530

    commit

--- IOP/LAB5&6/assignment47.c
+++ IOP/LAB5&6/assignment47.c
@@ -5,7 +5,7 @@
 // ABCD
 // ABCDE
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main() {
     int i,j ;
     char A,B,C,D,E;
@@ -23,4 +23,43 @@ int main() {
     }
     
     return 0;
+}*/
+
+#include <stdio.h>
+int main(){
+
+/*
+
+A
+A B
+A B 
+
+
+*/
+
+
+int ascii = 65, result;
+
+
+for (int i = 1; i <= 5 ; i++)
+{
+  for (int j = 1; j <= i; j++)
+  {
+    result = ascii;
+    printf(" %c ", result);
+    ascii += 1;
+  }
+    printf("\n");
+    ascii = 65;
+  
 }
+
+
+
+
+
+
+
+    return 0;
+}
+
--- IOP/LAB5&6/assignment54.c
+++ IOP/LAB5&6/assignment54.c
@@ -1,7 +1,18 @@
 // THIS IS PROGRAM TO RED AND ARRAY OF 10 INTEGER AND PRINT SUM OF NUMBERS.
 
 #include <stdio.h>
-int main() {
+int main()
+{
+    int num[10],sum=0,i ;
+    for ( i = 0; i < 10; i++)
+    {
+        printf("enter value of %d element : \n",i);
+        scanf("%d",&num[i]);
+        sum = sum +num[i] ;
+    }
+
+    printf("sum of all elements is %d",sum);
+    
     
     return 0;
 }
