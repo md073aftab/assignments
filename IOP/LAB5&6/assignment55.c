98241b7a42a5637854f8bfb0e5d9e3a3a95dc048
md073aftab <u24ai058@aid.svnit.ac.in>
2024-09-21 10:49:15 +0530

    commit

--- IOP/LAB5&6/assignment55.c
+++ IOP/LAB5&6/assignment55.c
@@ -1,22 +1,28 @@
-//  PROGRAM TO READ AN ARRAY OF 10 INTEGER AND COUNT TOTAL NO. OF POSITIVE, 
-//  NEGATIVE, AND ZERO ELEMENTS
-
-
-
+//  PROGRAM TO READ AN ARRAY OF 10 INTEGER AND COUNT TOTAL NO OF ODD AND 
+//  TOTAL NO. OF EVERY ELEMENT.
 
 #include <stdio.h>
-int main()
-{
-    int num[10],sum=0,i,pos_num=0 ;
+int main() {
+    int s_odd=0, s_even =0,arr[10],i ;
+
     for ( i = 0; i < 10; i++)
     {
-        printf("enter value of  element%d : \n",i);
-        scanf("%d",&num[i]);
-         
+        printf("enter the element %d : ",i);
+        scanf("%d",&arr[i]);
+
+        if (arr[i]%2==0)
+        {
+            s_even =s_even + arr[i];
+        }
+         if (arr[i]%2!=0)
+        {
+            s_odd =s_odd + arr[i];
+        }
     }
 
-    printf("sum of all elements is %d",sum);
+    printf("sum of all odd elements of array is : %d \n", s_odd);
+    printf("sum of all even elements of array is : %d\n", s_even);
     
-    
-    return 0;
+    return 0;2
+
 }
