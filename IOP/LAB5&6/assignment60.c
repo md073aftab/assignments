5211c4559ce07752c54529bb7d1c7b56b30eeaa0
md073aftab <u24ai058@aid.svnit.ac.in>
2024-10-03 15:24:59 +0530

    commit

--- IOP/LAB5&6/assignment60.c
+++ IOP/LAB5&6/assignment60.c
@@ -1,7 +1,31 @@
 //  PROGRAM TO SORT (SELECTION SORT) AN ARRAY
+selection sort#include<stdio.h>
+int main(){
+    int a[10],i;
+    int j,temp,num;
+    
+    printf("Enter the number to give\n");
+    scanf("%d",&num);
+    
+    for(i=0; i<num; i++){
+        printf("a[%d]=\t",i);
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0; i<num-1; i++){  
+        for(j=i+1;j<num; j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
 
-
-
-
-
-// pending
+printf("Selection Sort in C\n");
+for(i=0; i<num; i++){
+        printf("a[%d]=\t%d\n",i,a[i]);
+    }
+    
+    return 0;
+}
