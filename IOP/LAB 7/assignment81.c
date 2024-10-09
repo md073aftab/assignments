55f76ece083d42467c44c3bd415cc8c92619de4d
md073aftab <u24ai058@aid.svnit.ac.in>
2024-10-09 14:50:33 +0530

    commit

--- IOP/LAB 7/assignment81.c
+++ IOP/LAB 7/assignment81.c
@@ -1,1 +1,38 @@
 //  WRITE A FUNCTION THAT RETURN 1 IF ITS ARGUMENT IS PRIME NUMBER AND RETURNZERO OTHERWISE.
+
+#include <stdio.h>
+
+void prime(n)
+{
+    int flag=0,i;
+    if (n <= 1)
+    {
+        printf("0\n");
+    }
+
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;  
+            break;    
+        }
+    }
+
+    if (flag == 0)
+        printf("1\n");
+    else
+        printf("0\n");
+
+
+}
+int main() 
+{
+    int n;
+    printf("enter the number\n");
+    scanf("%d",&n);
+
+    prime(n);
+
+    return 0;
+}
