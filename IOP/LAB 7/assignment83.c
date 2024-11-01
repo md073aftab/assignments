4a346859f1d01e1b54eb26786c208c72869f4625
md073aftab <u24ai058@aid.svnit.ac.in>
2024-11-01 11:34:22 +0530

    commit

--- IOP/LAB 7/assignment83.c
+++ IOP/LAB 7/assignment83.c
@@ -1,1 +1,38 @@
-// 
+//  WRITE A FUNCTION TO READ AN ARRAY AND FOUND OUT MAXIMUM.
+
+#include <stdio.h>
+int main()
+{
+    int size,max ;
+    printf("enter the size of array : ");
+    scanf("%d",&size);
+
+    if (size<0)
+    {
+        printf("invalid array size");
+    }
+    else{
+    
+
+    int array[size];
+    for (int i = 0; i < size; i++)
+    {
+        printf("enter element %d of array : ",i);
+        scanf("%d",&array[i]);
+
+    }
+
+    max = array[0] ;
+
+    for (int i = 0; i < size; i++)
+    {
+      if (array[i]> max)       
+       {
+        max= array[i] ;
+       }
+    }
+    
+    printf("max element of array is : %d ", max);
+    return 0;
+    }
+}
